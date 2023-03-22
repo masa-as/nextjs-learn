bfbc9022ed56dae12c4910de1daa5d8fff1b94a0
masa-as <masashiiijima@gmail.com>
2023-03-22 18:12:17 +0900

    プロフィール画面作成

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,20 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import Head from 'next/head';
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2>
-        <Link href='/'>Back to home</Link>
-      </h2>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Layout>
+        <h1>First Post</h1>
+        <h2>
+          <Link href='/'>Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 }
